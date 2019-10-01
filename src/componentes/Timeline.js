f99e963ff47e5eec6bd67326ce42b6d1c2cf8d09
Robsondj <robson.djsilva@gmail.com>
2019-10-01 16:29:34 -0300

    Adicionado funcionalidades de Login e Logout a timeline

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -9,16 +9,18 @@ export default class Timeline extends Component {
 	}
 
 	componentDidMount() {
-		fetch('http://localhost:8080/api/public/fotos/rafael')
-		.then(response => response.json())
-		.then(fotos => this.setState({fotos: fotos}));
-	}
+		fetch(`http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`)
+	        .then(response => response.json())
+	        .then(fotos => {
+                this.setState({fotos:fotos});
+            });
+    }
 
     render(){
         return (
         <div className="fotos container">
           {
-          	this.state.fotos.map(foto => <FotoItem foto={foto}/>)
+          	this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto}/>)
           }
         </div>            
         );
