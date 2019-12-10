00cf3c24ec714b9f8f176c6f11aa358c71f2430e
Robsondj <robson.djsilva@gmail.com>
2019-12-10 09:34:59 -0300

    Alteração para implementar a metodologia do Flux utilizando o redux

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FotoItem from './FotoItem';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import TimelineApi from '../logicas/TimelineApi';
 
 export default class Timeline extends Component {
 
@@ -11,7 +12,7 @@ export default class Timeline extends Component {
 	}
 
 	componentWillMount() {
-		this.props.store.subscribe(fotos => this.setState({fotos}));
+		this.props.store.subscribe(() => this.setState({fotos:this.props.store.getState()}));
 	}
 
 	carregaFotos() {
@@ -24,7 +25,7 @@ export default class Timeline extends Component {
             urlPerfil = `http://localhost:8080/api/public/fotos/${this.login}`;
         }
 
-        this.props.store.lista(urlPerfil);
+        this.props.store.dispatch(TimelineApi.lista(urlPerfil));
 	}
 
 	componentDidMount() {
@@ -40,11 +41,11 @@ export default class Timeline extends Component {
     }
 
     like(fotoId) {
-    	this.props.store.like(fotoId);
+    	this.props.store.dispatch(TimelineApi.like(fotoId));
     }
 
     comenta(fotoId, textoComentario) {
-    	this.props.store.comenta(fotoId, textoComentario);
+    	this.props.store.dispatch(TimelineApi.comenta(fotoId, textoComentario));
     }
 
     render(){
